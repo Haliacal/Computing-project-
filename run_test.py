9afff292cd8aa85a73b2d8e7cd0a012ed6548acb
Haliacal <36955979+Haliacal@users.noreply.github.com>
2020-11-10 23:03:13 +0000

    Update run_test.py

--- run_test.py
+++ run_test.py
@@ -1,20 +1,39 @@
 # Packages
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Activation, Dense
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.metrics import categorical_crossentropy
-
 import quandl
+import pandas as pd
+import datetime as dt
 import numpy as np
-from random import randint
-from sklearn.utils import shuffle
+#import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
+from numpy import *
 
 quandl.ApiConfig.api_key = 'Gv9VqzUx_24QFyuG267H'
 
+# Variables
+count = 0
+mid_prices = []
 
-gold = quandl.get("LBMA/GOLD")
-np.array(gold)
-print(gold[:,0])
+# Imports data from quandl into an array
+gold_data = quandl.get("LBMA/GOLD",returns='numpy')
+
+
+gold_usd_open = gold_data['USD (AM)']
+gold_usd_close = np.nan_to_num(gold_data['USD (PM)'], nan=0.0)
+
+
+print(len(gold_usd_open))
+print(len(gold_usd_close))
+
+
+while (count < len(gold_usd_open)):
+    temp = (gold_usd_open[count] + gold_usd_close[count])/2.0
+
+    if(gold_usd_open[count] == 0 or gold_usd_close[count] == 0 ):
+        mid_prices.append(temp*2.0)
+
+    else:
+        mid_prices.append(temp)
+    count = count + 1
+
+print(mid_prices)
+
